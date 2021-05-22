9689686c81e5a30d6317867895bc4caaec30fa4b
Anthony Nolan <anthonynolan@gmail.com>
2021-05-22 16:48:01 +0100

    Number adding program

---
+++ first.py
@@ -0,0 +1,6 @@
+first_number = input('What is the first number?')
+print(f'the first number is {first_number}')
+
+second_number = input('What is the second number?')
+
+print(int(first_number) * int(second_number))
